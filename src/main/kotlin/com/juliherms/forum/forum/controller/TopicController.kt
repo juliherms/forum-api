20fc26c8bcadb3342601b2b4721da99c709cd2ba
Juliherms de Lima Vasconcelos <jlv@cesar-ldap.intranet>
2022-08-15 15:51:29 -0300

    feat: add update topic

--- src/main/kotlin/com/juliherms/forum/forum/controller/TopicController.kt
+++ src/main/kotlin/com/juliherms/forum/forum/controller/TopicController.kt
@@ -2,10 +2,12 @@ package com.juliherms.forum.forum.controller
 
 import com.juliherms.forum.forum.dto.NewTopicForm
 import com.juliherms.forum.forum.dto.TopicView
+import com.juliherms.forum.forum.dto.UpdateTopicForm
 import com.juliherms.forum.forum.service.TopicService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -28,5 +30,10 @@ class TopicController(private val service: TopicService) {
     @PostMapping
     fun create(@RequestBody @Valid dto: NewTopicForm) {
         service.create(dto)
+    }
+
+    @PutMapping
+    fun update(@RequestBody @Valid dto: UpdateTopicForm) {
+        service.update(dto)
     }
 }
--- src/main/kotlin/com/juliherms/forum/forum/service/TopicService.kt
+++ src/main/kotlin/com/juliherms/forum/forum/service/TopicService.kt
@@ -2,6 +2,7 @@ package com.juliherms.forum.forum.service
 
 import com.juliherms.forum.forum.dto.NewTopicForm
 import com.juliherms.forum.forum.dto.TopicView
+import com.juliherms.forum.forum.dto.UpdateTopicForm
 import com.juliherms.forum.forum.mapper.TopicViewMapper
 import com.juliherms.forum.forum.model.Topic
 import com.juliherms.forum.forum.mapper.NewTopicDTOMapper
@@ -34,5 +35,22 @@ class TopicService(
         val topic = newTopicDTOMapper.map(form)
         topic.id = topics.size.toLong() + 1
         topics = topics.plus(topic)
+    }
+
+    fun update(dto: UpdateTopicForm) {
+        val topic = topics.stream().filter{
+            t -> t.id == dto.id
+        }.findFirst().get()
+
+        topics = topics.minus(topic).plus(Topic(
+            id = dto.id,
+            title = dto.title,
+            message = dto.message,
+            author = topic.author,
+            course = topic.course,
+            answers = topic.answers,
+            status = topic.status,
+            createdDate = topic.createdDate
+        ))
     }
 }
