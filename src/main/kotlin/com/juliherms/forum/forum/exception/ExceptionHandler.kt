60b2b1ed390e6bf09d708b3bb21fbe4067705f64
Juliherms de Lima Vasconcelos <jlv@cesar-ldap.intranet>
2022-08-15 17:16:45 -0300

    feat: add custom bad request exceptions

--- src/main/kotlin/com/juliherms/forum/forum/exception/ExceptionHandler.kt
+++ src/main/kotlin/com/juliherms/forum/forum/exception/ExceptionHandler.kt
@@ -2,6 +2,7 @@ package com.juliherms.forum.forum.exception
 
 import com.juliherms.forum.forum.dto.ErrorView
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -21,6 +22,25 @@ class ExceptionHandler {
             path = request.servletPath
         )
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun handleValidationError(exception:MethodArgumentNotValidException,
+                              request: HttpServletRequest): ErrorView{
+
+        val errorMessage = HashMap<String,String?>()
+        exception.bindingResult.fieldErrors.forEach{
+            e -> errorMessage.put(e.field,e.defaultMessage)
+        }
+
+        return ErrorView(
+            status = HttpStatus.BAD_REQUEST.value(),
+            error = HttpStatus.BAD_REQUEST.name,
+            message = errorMessage.toString(),
+            path = request.servletPath
+        )
+    }
+
 
     @ExceptionHandler(Exception::class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
