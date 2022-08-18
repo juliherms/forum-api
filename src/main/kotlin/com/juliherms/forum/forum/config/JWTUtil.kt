9cb02734054c7207f15a74bfa0bcec46e110325b
Juliherms de Lima Vasconcelos <jlv@cesar-ldap.intranet>
2022-08-18 17:45:06 -0300

    feat: add jwt and filters for authentication

--- src/main/kotlin/com/juliherms/forum/forum/config/JWTUtil.kt
+++ src/main/kotlin/com/juliherms/forum/forum/config/JWTUtil.kt
@@ -1,9 +1,14 @@
 package com.juliherms.forum.forum.config
 
 import com.juliherms.forum.forum.service.UserService
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.Authentication
 import org.springframework.security.core.GrantedAuthority
 import org.springframework.stereotype.Component
+import java.util.Date
 
 @Component
 class JWTUtil(
@@ -15,6 +20,26 @@ class JWTUtil(
     private lateinit var secret: String
 
     fun generateToken(username:String, authorities: MutableCollection<out GrantedAuthority>): String? {
-       return null;
+        return Jwts.builder()
+            .setSubject(username)
+            .claim("role", authorities)
+            .setExpiration(Date(System.currentTimeMillis() + expiration))
+            .signWith(SignatureAlgorithm.HS512, secret.toByteArray())
+            .compact()
+    }
+
+    fun isValid(jwt: String?): Boolean {
+        return try {
+            Jwts.parser().setSigningKey(secret.toByteArray()).parseClaimsJws(jwt)
+            true
+        } catch (e: IllegalArgumentException) {
+            false
+        }
+    }
+
+    fun getAuthentication(jwt: String?): Authentication {
+        val username = Jwts.parser().setSigningKey(secret.toByteArray()).parseClaimsJws(jwt).body.subject
+        val user = userService.loadUserByUsername(username)
+        return UsernamePasswordAuthenticationToken(username, null, user.authorities)
     }
 }
--- src/main/kotlin/com/juliherms/forum/forum/config/SecurityConfiguration.kt
+++ src/main/kotlin/com/juliherms/forum/forum/config/SecurityConfiguration.kt
@@ -1,29 +1,51 @@
 package com.juliherms.forum.forum.config
 
+import com.juliherms.forum.forum.security.JWTAuthenticationFilter
+import com.juliherms.forum.forum.security.JWTLoginFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfiguration () {
+class SecurityConfiguration (
+    private val configuration: AuthenticationConfiguration,
+    private val jwtUtil: JWTUtil
+) {
 
     @Bean
     fun filterChain(http: HttpSecurity) : SecurityFilterChain {
-        return http.
-        authorizeHttpRequests().
-        anyRequest().
-        authenticated().
-        and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).
-        and().
-        formLogin().disable().
-        httpBasic().and().build()
+        http
+            .csrf().disable()
+            .authorizeHttpRequests()
+            .antMatchers(HttpMethod.POST, "/login").permitAll()
+            .antMatchers(HttpMethod.GET, "/swagger-ui/*").permitAll()
+            .antMatchers(HttpMethod.GET, "/v3/api-docs/**").permitAll()
+            .anyRequest()
+            .authenticated()
+            .and()
+            .addFilterBefore(
+                JWTLoginFilter(authManager = configuration.authenticationManager, jwtUtil = jwtUtil),
+                UsernamePasswordAuthenticationFilter::class.java
+            )
+
+        http.addFilterBefore(
+            JWTAuthenticationFilter(jwtUtil = jwtUtil),
+            UsernamePasswordAuthenticationFilter::class.java
+        )
+
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+        return  http.build()
     }
 
     fun encoder(): PasswordEncoder? {
